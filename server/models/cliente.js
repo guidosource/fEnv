a8319bbe3ce7160f165eb2a11ce83c2aa202bf8e
Guido <guido.vespignani@gmail.com>
2018-07-21 12:47:59 -0300

    commit

---
+++ server/models/cliente.js
@@ -0,0 +1,44 @@
+
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let schema = mongoose.Schema;
+
+let cliente = new Schema({
+
+    nombre : {
+        type : String,
+        required : [true, 'el nombre es necesario']
+    },
+    apellido : {
+        type : String,
+        required : [true, 'el apellido es necesario']
+    },
+    documento : {
+        type : Number,
+        unique : true
+    },
+    telefono : {
+        type : String,
+        unique : true,
+        required : [true, 'el telefono es necesario']
+    },
+    direccion : {
+        type : String,
+        unique : true,
+        required : [true, 'la dirección es necesaria']
+    },
+    email : {
+        type : String,
+        unique : true
+    },
+    estado : {
+        type : Boolean,
+        default: true
+    }
+
+});
+
+cliente.plugin(uniqueValidator, { message : '{PATH} no se puede repetir'});
+
+module.exports = mongoose.model('Cliente',cliente);
---
+++ server/server.js
@@ -0,0 +1,8 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
